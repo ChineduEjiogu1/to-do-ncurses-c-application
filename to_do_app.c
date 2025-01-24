9f634e29416e0b745bc58c30b960d10029f6d9fc
ChineduEjiogu1 <chineduejiogu@gmail.com>
2025-01-24 18:27:08 -0500

    trying to figure out memory leaks in create_task function.

--- to_do_app.c
+++ to_do_app.c
@@ -38,7 +38,8 @@ typedef struct Time
 // Sub-task structure
 typedef struct SubTask
 {
-    char *sub_task_description; // Sub-task description.
+    char *sub_task_description;     // Sub-task description.
+    struct SubTask *previous;     // Pointer to the previous sub-task (for linked list structure).
     struct SubTask *next;       // Pointer to the next sub-task (for linked list structure).
 } SubTask;
 
@@ -69,6 +70,8 @@ typedef struct Appointment
     char *description; // Appointment description.
     int day;           // Day of the month for the appointment.
     bool is_complete;  // New field to track completion status.
+    int appointment_duration; // how long appointment will last.
+    struct Task *task;
 } Appointment;
 
 // Node structure
@@ -102,4 +105,104 @@ char *strdup(const char *source)
         return NULL;
     }
     return (char *)memcpy(string, source, length);
+}
+
+Node *create_task(const char *task_description, CompletionLevel completion_level, bool is_subtask, bool is_recurring, bool have_reminder, 
+                    unsigned int priority, RecurrenceType recurrence_type, Time *minutes, Time *hours, const char *notes_description, 
+                    const char *reminder_description, SubTask *sub_task)
+{
+    Node *new_task = (Node *)malloc(sizeof(Node));
+
+    if (new_task == NULL)
+    {
+        return NULL;
+    }
+
+    new_task->task.task_description = NULL;
+    new_task->task.task_notes->notes_description = NULL;
+    new_task->task.reminder_description = NULL;
+    new_task->task.sub_task = NULL;
+
+    if (task_description != NULL)
+    {
+        new_task->task.task_description = strdup(task_description);
+
+        if (new_task->task.task_description)
+        {
+            free(new_task->task.task_description);
+        }
+    }
+
+    if (notes_description != NULL)
+    {
+        new_task->task.task_notes->notes_description = strdup(notes_description);
+
+        if (new_task->task.task_notes)
+        {
+            free(new_task->task.task_notes);
+        }
+        
+        free(new_task->task.task_notes->notes_description);
+    }
+
+    if (reminder_description != NULL)
+    {
+        new_task->task.reminder_description = strdup(reminder_description);
+
+        if (new_task->task.reminder_description)
+        {
+            free(new_task->task.reminder_description);
+        }
+    }
+
+    if (sub_task != NULL)
+    {
+        new_task->task.sub_task = sub_task;
+        free(sub_task);
+    }
+   
+    new_task->task.have_reminder = have_reminder;
+    new_task->task.is_recurring = is_recurring;
+    new_task->task.is_sub_task = is_recurring;
+    new_task->task.priority = priority;
+    new_task->task.recurrence_type = recurrence_type;
+    new_task->task.task_time = hours;
+    new_task->task.task_time = minutes;
+    new_task->task.completion_level = completion_level;
+
+    return new_task;
+}
+
+Calendar *create_calendar(Node *task, int day, int month, int year)
+{
+    Calendar *new_calendar = (Calendar *)malloc(sizeof(Calendar));
+
+    new_calendar->day = day;
+    new_calendar->month = month;
+    new_calendar->year = year;
+    new_calendar->tasks = task;
+
+    return new_calendar;
+}
+
+Appointment *create_appointment(int day, const char *appointment_description, bool is_complete, int appointment_duration, const char *time)
+{
+    Appointment *new_appointment = (Appointment *)malloc(sizeof(Appointment));
+
+    new_appointment->day = day;
+    
+    if (appointment_description != NULL)
+    {
+
+    }
+
+    new_appointment->appointment_duration;
+    new_appointment->time;
+
+    return new_appointment;
+}
+
+int main()
+{
+    return 0;
 }
